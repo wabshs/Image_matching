9829f49690e36ab3081356c1ed734169ed2fc21b
杜昱恒 <duyh1@digitalchina.com>
2023-10-25 18:05:20 +0800

    init vue

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 import os
 import cv2
-import time
-
-from datetime import timedelta
 
 from skimage.metrics import structural_similarity as ssim
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
@@ -33,36 +32,11 @@ def matching():
     return '请返回控制台查看相似度'
 
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'JPG', 'PNG', 'bmp'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'JPG', 'PNG'}
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-# 图片上传
-@app.route('/upload', methods=['POST', 'GET'])  # 添加路由
-def upload():
-    if request.method == 'POST':
-        f = request.files['file']
-
-        if not (f and allowed_file(f.filename)):
-            return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
-
-        user_input = request.form.get("name")
-
-        basepath = os.path.dirname(__file__)  # 当前文件所在路径
-
-        upload_path = os.path.join(basepath, 'static/images', secure_filename(f.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
-        f.save(upload_path)
-
-        # 使用Opencv转换一下图片格式和名称
-        img = cv2.imread(upload_path)
-        cv2.imwrite(os.path.join(basepath, 'static/images', 'test.jpg'), img)
-
-        return render_template('upload_ok.html', userinput=user_input, val1=time.time())
-
-    return render_template('upload.html')
 
 
 if __name__ == '__main__':
---
+++ image_matching_front/src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <link rel="stylesheet">
+
+    <!--    左边的部分-->
+    <div class="left-container">
+      <el-tag
+          key="pic1"
+          type="success"
+          effect="dark">
+        图片一
+      </el-tag>
+
+
+
+
+    </div>
+
+    <!--    右边的部分-->
+    <div class="right-container">
+      <el-tag
+          key="pic2"
+          type="success"
+          effect="dark">
+        图片二
+      </el-tag>
+      <el-upload
+          class="upload-demo"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件</div>
+      </el-upload>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+    }
+  },
+
+  methods: {}
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+.container {
+  display: flex;
+  height: 100vh;
+
+}
+
+.left-container,
+.right-container {
+  flex: 1;
+  border: 1px solid #ccc;
+}
+
+
+</style>
